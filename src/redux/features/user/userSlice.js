682d697842a7f192644f1fccc0af01dcb76b0eb1
Li Pon <mirnatul@gmail.com>
2023-09-26 02:59:05 +0600

    working in signup

--- src/redux/features/user/userSlice.js
+++ src/redux/features/user/userSlice.js
@@ -1,9 +1,17 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { createUserWithEmailAndPassword } from 'firebase/auth'
 
 const initialState = {
-  name: 'Mir Hussain',
-  email: 'mir@gmail.com',
+  name: '',
+  email: '',
+  isLoading: true,
+  isError: false,
+  error: '',
 };
+
+export const createUser = createAsyncThunk('userSlice/createUser', async () => {
+  createUserWithEmailAndPassword()
+})
 
 const userSlice = createSlice({
   name: 'userSlice',
